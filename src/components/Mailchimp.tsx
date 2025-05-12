e0d3ceda58a1df5d8e3fecf9f7fef20ef2bff6aa
Matthew Keresey <matt@dirigotechnology.com>
2025-05-11 23:53:08 -0400

    tailwind

--- src/components/Mailchimp.tsx
+++ src/components/Mailchimp.tsx
@@ -19,169 +19,51 @@ type NewsletterProps = {
   description: string | JSX.Element;
 };
 
-export const Mailchimp = ({ newsletter }: { newsletter: NewsletterProps }) => {
-  const [email, setEmail] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  const [touched, setTouched] = useState<boolean>(false);
-
-  const validateEmail = (email: string): boolean => {
-    if (email === "") {
-      return true;
-    }
-
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setEmail(value);
-
-    if (!validateEmail(value)) {
-      setError("Please enter a valid email address.");
-    } else {
-      setError("");
-    }
-  };
-
-  const debouncedHandleChange = debounce(handleChange, 2000);
-
-  const handleBlur = () => {
-    setTouched(true);
-    if (!validateEmail(email)) {
-      setError("Please enter a valid email address.");
-    }
-  };
-
+export const Mailchimp = () => {
   return (
-    <Column
-      overflow="hidden"
-      fillWidth
-      padding="xl"
-      radius="l"
-      marginBottom="m"
-      horizontal="center"
-      align="center"
-      background="surface"
-      border="neutral-alpha-weak"
-    >
-      <Background
-            position="absolute"
-            mask={{
-              x: mailchimp.effects.mask.x,
-              y: mailchimp.effects.mask.y,
-              radius: mailchimp.effects.mask.radius,
-              cursor: mailchimp.effects.mask.cursor
-            }}
-            gradient={{
-              display: mailchimp.effects.gradient.display,
-              opacity: mailchimp.effects.gradient.opacity as opacity,
-              x: mailchimp.effects.gradient.x,
-              y: mailchimp.effects.gradient.y,
-              width: mailchimp.effects.gradient.width,
-              height: mailchimp.effects.gradient.height,
-              tilt: mailchimp.effects.gradient.tilt,
-              colorStart: mailchimp.effects.gradient.colorStart,
-              colorEnd: mailchimp.effects.gradient.colorEnd,
-            }}
-            dots={{
-              display: mailchimp.effects.dots.display,
-              opacity: mailchimp.effects.dots.opacity as opacity,
-              size: mailchimp.effects.dots.size as SpacingToken,
-              color: mailchimp.effects.dots.color,
-            }}
-            grid={{
-              display: mailchimp.effects.grid.display,
-              opacity: mailchimp.effects.grid.opacity as opacity,
-              color: mailchimp.effects.grid.color,
-              width: mailchimp.effects.grid.width,
-              height: mailchimp.effects.grid.height,
-            }}
-            lines={{
-              display: mailchimp.effects.lines.display,
-              opacity: mailchimp.effects.lines.opacity as opacity,
-              size: mailchimp.effects.lines.size as SpacingToken,
-              thickness: mailchimp.effects.lines.thickness,
-              angle: mailchimp.effects.lines.angle,
-              color: mailchimp.effects.lines.color,
-            }}
-          />
-      <Heading style={{ position: "relative" }} marginBottom="s" variant="display-strong-xs">
-        {newsletter.title}
-      </Heading>
-      <Text
-        style={{
-          position: "relative",
-          maxWidth: "var(--responsive-width-xs)",
+    <div id="mc_embed_shell">
+      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
+      <style>{`
+        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width: 100%; max-width: 600px; margin: 0 auto;}
+      `}</style>
+      <div id="mc_embed_signup">
+        <form action="https://matthewkeresey.us20.list-manage.com/subscribe/post?u=cb398a49b20c7906f23664856&amp;id=b696a921fc&amp;f_id=009b5eeef0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+          <div id="mc_embed_signup_scroll">
+            <h2>Subscribe</h2>
+            <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+            <div className="mc-field-group">
+              <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label>
+              <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required />
+            </div>
+            <div id="mce-responses" className="clear foot">
+              <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+              <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+            </div>
+            <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px' }}>
+              <input type="text" name="b_cb398a49b20c7906f23664856_b696a921fc" tabIndex={-1} value="" readOnly />
+            </div>
+            <div className="optionalParent">
+              <div className="clear foot">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
+                <p style={{ margin: '0px auto' }}>
+                  <a href="http://eepurl.com/jex64s" title="Mailchimp - email marketing made easy and fun">
+                    <span style={{ display: 'inline-block', backgroundColor: 'transparent', borderRadius: '4px' }}>
+                      <img className="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style={{ width: '220px', height: '40px', display: 'flex', padding: '2px 0px', justifyContent: 'center', alignItems: 'center' }} />
+                    </span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';fnames[6]='COMPANY';ftypes[6]='text';}(jQuery));var $mcj = jQuery.noConflict(true);`
         }}
-        wrap="balance"
-        marginBottom="l"
-        onBackground="neutral-medium"
-      >
-        {newsletter.description}
-      </Text>
-      <form
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-        }}
-        action={mailchimp.action}
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-      >
-        <Flex id="mc_embed_signup_scroll" fillWidth maxWidth={24} mobileDirection="column" gap="8">
-          <Input
-            formNoValidate
-            labelAsPlaceholder
-            id="mce-EMAIL"
-            name="EMAIL"
-            type="email"
-            label="Email"
-            required
-            onChange={(e) => {
-              if (error) {
-                handleChange(e);
-              } else {
-                debouncedHandleChange(e);
-              }
-            }}
-            onBlur={handleBlur}
-            errorMessage={error}
-          />
-          <div style={{ display: "none" }}>
-            <input
-              type="checkbox"
-              readOnly
-              name="group[3492][1]"
-              id="mce-group[3492]-3492-0"
-              value=""
-              checked
-            />
-          </div>
-          <div id="mce-responses" className="clearfalse">
-            <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
-            <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
-          </div>
-          <div aria-hidden="true" style={{ position: "absolute", left: "-5000px" }}>
-            <input
-              type="text"
-              readOnly
-              name="b_c1a5a210340eb6c7bff33b2ba_0462d244aa"
-              tabIndex={-1}
-              value=""
-            />
-          </div>
-          <div className="clear">
-            <Flex height="48" vertical="center">
-              <Button id="mc-embedded-subscribe" value="Subscribe" size="m" fillWidth>
-                Subscribe
-              </Button>
-            </Flex>
-          </div>
-        </Flex>
-      </form>
-    </Column>
+      />
+    </div>
   );
 };
